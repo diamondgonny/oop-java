5416f0e8326e17bb892c7c45a7652a2f2f2daee3
Ernest Kim <diamondgonny@gmail.com>
2022-09-19 11:41:06 +0900

    Add ListItem.java with member variables

---
+++ Lab3/src/academy/pocu/comp2500/lab3/ListItem.java
@@ -0,0 +1,11 @@
+package academy.pocu.comp2500.lab3;
+
+import java.util.ArrayList;
+
+public class ListItem {
+    private String text;
+    private ArrayList<ListItem> sublistItems = new ArrayList<>();
+    private char bulletStyle;
+
+
+}
