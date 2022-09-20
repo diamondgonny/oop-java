737ef65e57d98630c9e70fc003772208d32c08e3
Ernest Kim <diamondgonny@gmail.com>
2022-09-20 11:07:21 +0900

    Update App.Java

--- Assignment1/src/academy/pocu/comp2500/assignment1/App.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/App.java
@@ -6,5 +6,26 @@ public class App {
     public App(Registry registry) {
         // Register like this
         // registry.registerPostAdder("Foo", "bar");
+        registry.registerBlogCreator("Blog");
+        registry.registerTagFilterSetter("Blog", "setTagFilter");
+        registry.registerAuthorFilterSetter("Blog", "setAuthorFilter");
+        registry.registerPostOrderSetter("Blog", "setPostOrder");
+        registry.registerPostListGetter("Blog", "getPostList");
+        registry.registerPostAdder("Blog", "addPost");
+        registry.registerPostTitleUpdater("Post", "setPostTitle");
+        registry.registerPostBodyUpdater("Post", "setPostBody");
+        registry.registerPostTagAdder("Post", "setPostTag");
+        registry.registerReactionAdder("Post", "addReaction");
+        registry.registerReactionRemover("Post", "removeReaction");
+        registry.registerCommentAdder();
+        registry.registerCommentUpdater();
+        registry.registerCommentListGetter();
+        registry.registerSubcommentAdder("Comment", "addSubcomment");
+        registry.registerSubcommentUpdater("Comment", "setSubcomment");
+        registry.registerCommentUpvoter("Comment", "addUpvote");
+        registry.registerCommentDownvoter("Comment", "addDownvote");
+        registry.registerSubcommentListGetter("Comment", "getSubcommentList");
+        registry.registerSubcommentUpvoter("Comment", "addUpvote");
+        registry.registerSubcommentDownvoter("Comment", "addDownvote");
     }
 }
