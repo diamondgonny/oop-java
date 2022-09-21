ff9ba2f922a4c9ade9bc2225bef8ad9dc0e6e986
Ernest Kim <diamondgonny@gmail.com>
2022-09-21 17:31:43 +0900

    Update overall codes in Post.java

--- Assignment1/src/academy/pocu/comp2500/assignment1/Post.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Post.java
@@ -2,6 +2,7 @@ package academy.pocu.comp2500.assignment1;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Post {
@@ -12,7 +13,8 @@ public class Post {
     private OffsetDateTime updatedDateTime;
     private HashSet<String> tags;
     private ArrayList<Comment> comments;
-    private ReactionType reactionType;
+    private HashMap<ReactionType, HashSet<User>> reactions;
+    // private ReactionType reactionType;
 
     private enum ReactionType {
         GREAT,
@@ -30,44 +32,104 @@ public class Post {
         this.updatedDateTime = createdDateTime;
         this.tags = new HashSet<>();
         this.comments = new ArrayList<>();
-        // *reactionType?
+        this.reactions = new HashMap<>();
+        // *reactions는 HashMap? ArrayList? (User의 고유성)  /  .values, .put(HashMap)
+        for (ReactionType reactionType : ReactionType.values()) {
+            reactions.put(reactionType, new HashSet<>());
+        }
     }
 
-    public String getAuthor() {
-        return this.author.getName();
+    public String getTitle() {
+        return this.title;
+    }
+
+    public User getAuthor() {
+        return this.author;
     }
 
     public OffsetDateTime getCreatedDateTime() {
-        return createdDateTime;
+        return this.createdDateTime;
     }
 
     public OffsetDateTime getUpdatedDateTime() {
-        return updatedDateTime;
+        return this.updatedDateTime;
     }
 
-    // setPostTitle
-    public void setPostTitle(String title) {
+    public HashSet<String> getTags() {
+        return this.tags;
+    }
+
+    // setPostTitle(... boolean?)
+    public boolean setPostTitle(User author, String title) {
+        if(this.author.equals(author)) {
+            return false;
+        }
+        this.updatedDateTime = OffsetDateTime.now();
         this.title = title;
+        return true;
     }
 
-    // setPostBody
-    public void setPostBody(String body) {
+    // setPostBody(")
+    public boolean setPostBody(User author, String body) {
+        if(this.author.equals(author)) {
+            return false;
+        }
+        this.updatedDateTime = OffsetDateTime.now();
         this.body = body;
+        return true;
     }
 
-    // addPostTag
+    // *addPostTag??? setPostTags???
     public void addPostTag(String tag) {
         this.tags.add(tag);
     }
 
-    // addReaction
+    public boolean checkReactionExists(ReactionType reactionType) {
+        for (ReactionType reaction : ReactionType.values()) {
+            if (reactionType.equals(reaction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // addReaction / .get(HashMap?)
+    public boolean addReaction(User user, ReactionType reactionType) {
+        if (checkReactionExists(reactionType) == false) {
+            return false;
+        }
+        HashSet<User> reactionUsers = this.reactions.get(reactionType);
+        if (reactionUsers.contains(user) == true) {
+            return false;
+        }
+        reactionUsers.add(user);
+        return true;
+    }
 
     // removeReaction
+    public boolean removeReaction(User user, ReactionType reactionType) {
+        if (checkReactionExists(reactionType) == false) {
+            return false;
+        }
+        HashSet<User> reactionUsers = this.reactions.get(reactionType);
+        if (reactionUsers.contains(user) == false) {
+            return false;
+        }
+        reactionUsers.remove(user);
+        return true;
+    }
 
-    // addComment
+    // addComment : boolean?
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
 
-    // setComment
+    // *setComment : Comment로 보낼 듯 (댓글 수정)
+    public void setComment(Comment comment) {
+        // -----------------------
+    }
 
-    // getCommentlist
+    // getCommentlist : Upvotes - Downvotes 차이에 의한 정렬 req.
+    //public ArrayList<Comment> getCommentlist() {}
 
 }
