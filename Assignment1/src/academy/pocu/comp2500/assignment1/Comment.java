1a9be7ba3dcce17dbaad47ed92cfb0dc5a742fa5
Ernest Kim <diamondgonny@gmail.com>
2022-09-21 22:10:07 +0900

    Update(setup) Comment.java -> Draft

--- Assignment1/src/academy/pocu/comp2500/assignment1/Comment.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Comment.java
@@ -1,34 +1,66 @@
 package academy.pocu.comp2500.assignment1;
 
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class Comment {
-    private String text;
-    private ArrayList<Comment> subcomments = new ArrayList<>();
-    private HashSet<User> upvoters = new HashSet<>();
-    private HashSet<User> downvoters = new HashSet<>();
     private User author;
-    private OffsetDateTime createdDateTime;
-    private OffsetDateTime updatedDateTime;
+    private String text;
+    private ArrayList<Comment> subcomments;
+    private HashSet<User> upvoters;
+    private HashSet<User> downvoters;
 
     public Comment(User author, String text) {
         this.author = author;
         this.text = text;
-        this.createdDateTime = OffsetDateTime.now();
-        this.updatedDateTime = createdDateTime;
-        // subcomments, upvoters, downvoters?
+        this.subcomments = new ArrayList<>();
+        this.upvoters = new HashSet<>();
+        this.downvoters = new HashSet<>();
+    }
+
+    // setCommentFamily
+    public boolean setCommentFamily(User author, String text) {
+        if (this.author.equals(author) == false) {
+            return false;
+        }
+        this.text = text;
+        return true;
     }
 
     // addSubcomment
+    public void addSubcomment(Comment subcomment) {
+        this.subcomments.add(subcomment);
+    }
 
-    // setSubcomment
-
-    // getSubcommentList
+    // getSubcommentListWithSort
+    public ArrayList<Comment> getSubcommentListWithSort() {
+        Collections.sort(this.subcomments, (s1, s2) -> s1.countVotePoints() - s2.countVotePoints());
+        return this.subcomments;
+    }
 
     // addUpvoter
+    public boolean addUpvoter(User user) {
+        if (upvoters.contains(user) == true) {
+            return false;
+        }
+        this.upvoters.add(user);
+        return true;
+    }
 
     // addDownvoter
+    public boolean addDownvoter(User user) {
+        if (upvoters.contains(user) == true) {
+            return false;
+        }
+        this.downvoters.add(user);
+        return true;
+    }
 
+    // countVotes
+     int countVotePoints() {
+        int upvote = this.upvoters.size();
+        int downvote = this.downvoters.size();
+        return upvote - downvote;
+    }
 }
