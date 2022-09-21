745b908bbbb8d97979574113825f71ca2c55d7b7
Ernest Kim <diamondgonny@gmail.com>
2022-09-21 22:01:11 +0900

    Updated App.java

--- Assignment1/src/academy/pocu/comp2500/assignment1/App.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/App.java
@@ -7,24 +7,24 @@ public class App {
         // Register like this
         // registry.registerPostAdder("Foo", "bar");
         registry.registerBlogCreator("Blog");
-        registry.registerPostAdder("Blog", "addPost");
         registry.registerTagFilterSetter("Blog", "setTagFilter");
         registry.registerAuthorFilterSetter("Blog", "setAuthorFilter");
         registry.registerPostOrderSetter("Blog", "setPostOrder");
+        registry.registerPostAdder("Blog", "addPost");
         registry.registerPostListGetter("Blog", "getPostList");
 
         registry.registerPostTitleUpdater("Post", "setPostTitle");
         registry.registerPostBodyUpdater("Post", "setPostBody");
         registry.registerPostTagAdder("Post", "setPostTags");
         registry.registerCommentAdder("Post", "addComment");
-        registry.registerCommentListGetter("Post", "getCommentList");
+        registry.registerCommentListGetter("Post", "getCommentListWithSort");
         registry.registerReactionAdder("Post", "addReaction");
         registry.registerReactionRemover("Post", "removeReaction");
 
-        registry.registerCommentUpdater("Comment", "setComment");
+        registry.registerCommentUpdater("Comment", "setCommentFamily");
+        registry.registerSubcommentUpdater("Comment", "setCommentFamily");
         registry.registerSubcommentAdder("Comment", "addSubcomment");
-        registry.registerSubcommentUpdater("Comment", "setSubcomment");
-        registry.registerSubcommentListGetter("Comment", "getSubcommentList");
+        registry.registerSubcommentListGetter("Comment", "getSubcommentListWithSort");
         registry.registerCommentUpvoter("Comment", "addUpvoter");
         registry.registerCommentDownvoter("Comment", "addDownvoter");
         registry.registerSubcommentUpvoter("Comment", "addUpvoter");
