50b2f7b47ff4d19d136e969e0002dd22e009502d
Ernest Kim <diamondgonny@gmail.com>
2022-09-30 17:23:45 +0900

    Update MemoryCache.java

--- Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
+++ Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
@@ -1,33 +1,52 @@
 package academy.pocu.comp2500.lab4;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MemoryCache {
-    private static List<String, String> instances;
-    private EvictionPolicy evictionPolicy = EvictionPolicy.LEAST_RECENTLY_USED;
-    private int key;
-    private int value;
-    private int maxInstanceCount;
+    private static HashMap<String, MemoryCache> instanceMap = new HashMap<String, MemoryCache>();
+    private static LinkedList<String> instanceList = new LinkedList<String>();
+    private static int maxInstanceCount = Integer.MAX_VALUE;   // *static* 개체가 몇 개 인가?
+    // private EvictionPolicy evictionPolicy = EvictionPolicy.LEAST_RECENTLY_USED;
 
     private MemoryCache() {
     }
 
     public static MemoryCache getInstance(String myHardDiskName) {
-        // 지정된 하드 디스크 전용 MemoryCache 인스턴스가 존재하지 않는다면 새로 생성하여 반환
-        // 만약 현재 사용 중인 MemoryCache 인스턴스의 수가 최대 허용치를 넘으면 가장 오래된(LRU) 인스턴스를 제거
-        // return instances;
-        if
+        // 인스턴스 신규 등록 or 기존 등록
+       if (instanceMap.containsKey(myHardDiskName) == false) {
+           instanceMap.put(myHardDiskName, new MemoryCache());
+           instanceList.addFirst(myHardDiskName);
+           // 만약 현재 사용 중인 MemoryCache 인스턴스의 수가 최대 허용치를 넘으면? 가장 오래된(LRU) 인스턴스 제거
+           MemoryCache.removeExceedingMaxInstance();
+        } else {
+           // 최근 순서로 끌어오기(LRU)
+           instanceList.remove(myHardDiskName);
+           instanceList.addFirst(myHardDiskName);
+        }
+        return instanceMap.get(myHardDiskName);
     }
 
     public static void clear() {
-        // 모든 인스턴스 캐시 제거
-        instances = null;
+        instanceMap.clear();       // 하위 항목들도 clear 처리 (cf. 메모리누수?)
+        // ...
     }
 
-    public void setMaxInstanceCount(int maxInstanceCount) {
+    public static void setMaxInstanceCount(int maxInstanceCount) {
+        MemoryCache.maxInstanceCount = maxInstanceCount;
         // 만약 현재 사용 중인 MemoryCache 인스턴스의 수가 최대 허용치를 넘게 되면 LRU에 기초하여 초과분을 제거
+        MemoryCache.removeExceedingMaxInstance();
     }
 
+    // private 메서드는 맨 아래로
+    private static void removeExceedingMaxInstance() {
+        while (instanceMap.size() > maxInstanceCount) {
+            // instanceMap.remove(?);
+        }
+    }
+
+
+    /*
     public void setEvictionPolicy(EvictionPolicy evictionPolicy) {
         switch(this.evictionPolicy) {
             case FIRST_IN_FIRST_OUT:
@@ -58,5 +77,6 @@ public class MemoryCache {
     public void setMaxEntryCount(int maxEntryCount) {
         // 현재 캐시에 저장되어 있는 항목수가 최대 허용치보다 크다면, 사용중인 캐시 퇴거 정책에 따라 초과분 제거
     }
+    */
 
 }
