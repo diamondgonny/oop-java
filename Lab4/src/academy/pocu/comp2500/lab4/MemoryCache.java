7a2aedd8dc6497d5db2aa8ad6bc49e0db202c9d1
Ernest Kim <diamondgonny@gmail.com>
2022-10-01 12:24:05 +0900

    Updated bunches of entry part in MemoryCache.java

--- Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
+++ Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
@@ -42,10 +42,12 @@ public class MemoryCache {
         MemoryCache.removeExceedingMaxInstance();
     }
 
+
     public void setEvictionPolicy(EvictionPolicy evictionPolicy) {
         this.evictionPolicy = evictionPolicy;
     }
 
+    // 시간복잡도
     public void addEntry(String key, String value) {
         if (entryMap.containsKey(key) == false) {
             entryMap.put(key, value);
@@ -53,13 +55,21 @@ public class MemoryCache {
             entryListUsedOrder.addFirst(key);
             this.removeExceedingMaxEntry();
         } else {
+            entryMap.put(key, value);
             entryListUsedOrder.remove(key);
             entryListUsedOrder.addFirst(key);
         }
     }
 
+    // 시간복잡도
     public String getEntryOrNull(String key) {
-        return entryMap.get(key);
+        if (entryMap.containsKey(key) == false) {
+            return null;
+        } else {
+            entryListUsedOrder.remove(key);
+            entryListUsedOrder.addFirst(key);
+            return entryMap.get(key);
+        }
     }
 
     public void setMaxEntryCount(int maxEntryCount) {
@@ -68,37 +78,39 @@ public class MemoryCache {
     }
 
     private static void removeExceedingMaxInstance() {
+        int index;
+        String key = null;
         while (instanceMap.size() > maxInstanceCount) {
-            int lastIndex = instanceList.size() - 1;
-            String key = instanceList.get(lastIndex);
-            instanceList.remove(lastIndex);
+            index = instanceList.size() - 1;
+            key = instanceList.get(index);
+            instanceList.remove(index);
             instanceMap.remove(key);
         }
     }
 
     private void removeExceedingMaxEntry() {
-        if (this.evictionPolicy == FIRST_IN_FIRST_OUT) {
-            while (entryMap.size() > maxEntryCount) {
-                int lastIndex = entryListAddedOrder.size() - 1;
-                String key = entryListAddedOrder.get(lastIndex);
-                entryListAddedOrder.remove(lastIndex);
-                entryMap.remove(key);
+        int index;
+        String key = null;
+        while (entryMap.size() > maxEntryCount) {
+            if (this.evictionPolicy == FIRST_IN_FIRST_OUT) {
+                index = entryListAddedOrder.size() - 1;
+                key = entryListAddedOrder.get(index);
+                entryListAddedOrder.remove(index);
+                entryListUsedOrder.remove(key);
+            } else if (this.evictionPolicy == LAST_IN_FIRST_OUT) {
+                index = 1;
+                key = entryListAddedOrder.get(index);
+                entryListAddedOrder.remove(key);
+                entryListUsedOrder.remove(key);
+            } else if (this.evictionPolicy == LEAST_RECENTLY_USED) {
+                index = entryListUsedOrder.size() - 1;
+                key = entryListUsedOrder.get(index);
+                entryListUsedOrder.remove(index);
+                entryListAddedOrder.remove(key);
+            } else {
+                assert (false) : "Unknown case evictionPolicy in 'removeExceedingMaxEntry' method";
             }
-        } else if (this.evictionPolicy == LAST_IN_FIRST_OUT) {
-            while (entryMap.size() > maxEntryCount) {
-                String key = entryListAddedOrder.get(0);
-                entryListAddedOrder.remove(0);
-                entryMap.remove(key);
-            }
-        } else if (this.evictionPolicy == LEAST_RECENTLY_USED) {
-            while (entryMap.size() > maxEntryCount) {
-                int lastIndex = entryListUsedOrder.size() - 1;
-                String key = instanceList.get(lastIndex);
-                instanceList.remove(lastIndex);
-                instanceMap.remove(key);
-            }
-        } else {
-            assert (false) : "Unknown case evictionPolicy in 'removeExceedingMaxEntry' method";
+            entryMap.remove(key);
         }
     }
 }
