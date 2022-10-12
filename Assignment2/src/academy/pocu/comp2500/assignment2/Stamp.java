95b8e0e023ef3cf2ba147f573aa91367488a03c2
gun0code <diamondgonny@gmail.com>
2022-10-12 11:28:10 +0900

    Remove duplicated methods in the class 'Stamp'

--- Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
@@ -8,20 +8,14 @@ public class Stamp extends Product {
         this.text = text;
     }
 
+    /*
     public void setSize(StampSize size) {
         this.setSize(size.getSize());
         this.setPrice(size.getPrice());
     }
-
-    public void setColor(StampColor color) {
-        this.setColor(color.getColor());
-    }
+     */
 
     public void setText(String text) {
         this.text = text;
-    }
-
-    public void setShippingMethod(ShippingMethod shippingMethod) {
-        this.setShippingMethod(shippingMethod);
     }
 }
