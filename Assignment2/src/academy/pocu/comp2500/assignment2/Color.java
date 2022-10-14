4d73305055ae5083083fcb94714d4cd79ca1a36b
gun0code <diamondgonny@gmail.com>
2022-10-14 10:27:33 +0900

    Simplify the method getRgbColor

--- Assignment2/src/academy/pocu/comp2500/assignment2/Color.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Color.java
@@ -13,7 +13,6 @@ public class Color {
 
     public int getRgbColor() {
         // How to use : System.out.format("0x%06x\n", item.getColor().getRgbColor());
-        int color = (int) red * 0x10000 + (int) green * 0x100 + (int) blue;
-        return color;
+        return (int) red * 0x10000 + (int) green * 0x100 + (int) blue;
     }
 }
