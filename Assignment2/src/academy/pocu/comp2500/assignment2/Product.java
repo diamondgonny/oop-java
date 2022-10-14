e13828a4d0275fd5b1a65965a09a091b6eab6bf2
gun0code <diamondgonny@gmail.com>
2022-10-14 17:00:51 +0900

    Remove unnecessary setter

--- Assignment2/src/academy/pocu/comp2500/assignment2/Product.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Product.java
@@ -35,22 +35,6 @@ public class Product {
         this.shippingMethod = shippingMethod;
     }
 
-    protected void setName(String name) {
-        this.name = name;
-    }
-
-    protected void setSize(Size size) {
-        this.size = size;
-    }
-
-    protected void setColor(Color color) {
-        this.color = color;
-    }
-
-    protected void setPrice(int price) {
-        this.price = price;
-    }
-
     protected void addPrice(int price) {
         this.price += price;
     }
