39aebcd76088d6352c211cc65613ebf484c97530
gun0code <diamondgonny@gmail.com>
2022-11-06 10:59:13 +0900

    Fix the newline in toString()

--- Lab7/src/academy/pocu/comp2500/lab7/ReadingList.java
+++ Lab7/src/academy/pocu/comp2500/lab7/ReadingList.java
@@ -23,8 +23,11 @@ public class ReadingList {
         StringBuilder sb = new StringBuilder();
         int num = 0;
         for (Book book : readingList) {
+            if (num != 0) {
+                sb.append(String.format("\n"));
+            }
             ++num;
-            sb.append(String.format("%d. %s\n", num, book.toString()));
+            sb.append(String.format("%d. %s", num, book.toString()));
         }
         return sb.toString();
     }
