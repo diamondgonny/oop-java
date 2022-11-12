d205a0c75bf0ea7705f62b740a57d2725d432d5f
gun0code <diamondgonny@gmail.com>
2022-11-12 15:26:24 +0900

    Update SimulationManager.java

--- Assignment3/src/academy/pocu/comp2500/assignment3/SimulationManager.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/SimulationManager.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 
 public final class SimulationManager {
     private static SimulationManager instance;
-    private final ArrayList<Unit> units;
-    private final ArrayList<IThinkable> thinkables;
-    private final ArrayList<IMovable> movables;
-    private final ArrayList<IListener> listeners;
+    private final UnitPositionManager unitPositionManager;
+    private final ArrayList<Unit> units = new ArrayList<>();
+    private final ArrayList<IThinkable> thinkables = new ArrayList<>();
+    private final ArrayList<IMovable> movables = new ArrayList<>();
+    private final ArrayList<IListener> listeners = new ArrayList<>();
 
     private SimulationManager() {
-        this.units = new ArrayList<>();
-        this.thinkables = new ArrayList<>();
-        this.movables = new ArrayList<>();
-        this.listeners = new ArrayList<>();
+        UnitPositionManager.createInstance();
+        this.unitPositionManager = UnitPositionManager.getInstance();
     }
 
     public static SimulationManager getInstance() {
@@ -29,6 +28,7 @@ public final class SimulationManager {
 
     public void spawn(Unit unit) {
         this.units.add(unit);
+        unitPositionManager.add(unit);
         unit.onSpawn(); // for register interfaces
     }
 
@@ -51,5 +51,34 @@ public final class SimulationManager {
         // 4) 각 유닛에게 공격할 기회를 줌
         // 5) 피해를 입어야 하는 각 유닛에게 피해를 입힘
         // 6) 죽은 유닛들을 모두 게임에서 제거함
+
+        for (IThinkable thinkable : this.thinkables) {
+            thinkable.think();
+        }
+        for (IMovable movable : this.movables) {
+            movable.move();
+        }
+        for (IListener listener : this.listeners) {
+            listener.listenCollisionEvent();
+        }
+        for (Unit unit : this.units) {
+            unit.attack();
+            // unit.onAttacked(damage);
+        }
+        for (Unit unit : this.units) {
+            if (unit.getHp() == 0) {
+                this.units.remove(unit);
+                unitPositionManager.remove(unit);
+                if (unit.getSymbol() != 'N' && unit.getSymbol() != 'A') {
+                    this.thinkables.remove(unit);
+                }
+                if (unit.getSymbol() != 'N' && unit.getSymbol() != 'A' && unit.getSymbol() != 'U') {
+                    this.movables.remove(unit);
+                }
+                if (unit.getSymbol() == 'N' && unit.getSymbol() != 'A') {
+                    this.listeners.remove(unit);
+                }
+            }
+        }
     }
 }
