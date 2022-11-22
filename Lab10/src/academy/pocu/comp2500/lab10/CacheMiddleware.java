39f644111df6c530b429ab890a886ec33e88e5d9
gun0code <diamondgonny@gmail.com>
2022-11-22 15:59:43 +0900

    Update CacheMiddleware method

--- Lab10/src/academy/pocu/comp2500/lab10/CacheMiddleware.java
+++ Lab10/src/academy/pocu/comp2500/lab10/CacheMiddleware.java
@@ -1,30 +1,36 @@
 package academy.pocu.comp2500.lab10;
 
 import academy.pocu.comp2500.lab10.pocuflix.ResultBase;
+import academy.pocu.comp2500.lab10.pocuflix.ResultCode;
+
+import java.util.HashMap;
 
 public class CacheMiddleware implements IRequestHandler {
     private IRequestHandler requestHandler;
     private int expiryDuration;
-    private int remainedExpiryCount;
-    // store
+    private HashMap<Request, Integer> caches;
 
     public CacheMiddleware(IRequestHandler requestHandler, int expiryDuration) {
         this.requestHandler = requestHandler;
         this.expiryDuration = expiryDuration;
-        this.remainedExpiryCount = 0;
+        this.caches = new HashMap<>();
     }
 
     @Override
     public ResultBase handle(Request request) {
         // 요청이 캐시에 저장되어 있었다면 CachedResult 개체를 반환합니다. [-1]
         // 그게 아니라면 다음 핸들러에 요청을 보냅니다. (이 미들웨어는 결과가 OkResult인 요청만 캐시에 저장합니다.) [추가/삭제]
-/*
-        if (0 < this.remainedExpiryCount && this.remainedExpiryCount <= expiryDuration) {
-            --expiryDuration;
-            return new CachedResult(this.remainedExpiryCount);
-        } else if (this.remainedExpiryCount == 0) {
 
+        // 캐시에 저장된 요청 (참고 : 1회 남은 걸 사용 == 만료)
+        if (caches.containsKey(request) && caches.get(request) > 1) {
+            caches.replace(request, caches.get(request) - 1);
+            return new CachedResult(caches.get(request));
         }
-*/
+
+        // 캐시에 저장 안된 요청 (신규 / 만료)
+        if (this.requestHandler.handle(request).getCode().equals(ResultCode.OK)) {
+            this.caches.put(request, expiryDuration);
+        }
+        return this.requestHandler.handle(request);
     }
 }
